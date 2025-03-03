b2f3c04a5ceba39def61e2457e5854f716c69452
ysugarmen <sugaryoni3@gmail.com>
2025-03-03 16:59:07 +0100

    Fixed CORS middleware

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ logger.info("🚀 Bet Manager is starting up...")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
